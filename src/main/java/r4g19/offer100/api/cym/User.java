143dff553ca4904415ed5ef35feae30cbb23c9a6
cym <16201235@stu.nchu.edu.cn>
2019-09-30 16:54:40 +0800

    HATEOAS的一小部分完成了，message.properties坏掉了

--- src/main/java/r4g19/offer100/api/cym/User.java
+++ src/main/java/r4g19/offer100/api/cym/User.java
@@ -1,8 +1,10 @@
 package r4g19.offer100.api.cym;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import r4g19.offer100.api.APIBase;
 
 @RestController
-public class PublicAPI extends APIBase {
+@RequestMapping("api/user")
+public class User extends APIBase {
 }
--- src/main/java/r4g19/offer100/model/EmptyEntity.java
+++
@@ -1,6 +0,0 @@
-package r4g19.offer100.model;
-
-import org.springframework.hateoas.RepresentationModel;
-
-public class EmptyEntity extends RepresentationModel {
-}
