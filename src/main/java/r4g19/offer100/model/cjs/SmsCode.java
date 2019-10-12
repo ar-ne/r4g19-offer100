9a1894ce89bf6de1c8ab01f560193e698ccb1274
yuyin <1721463423@qq.com>
2019-10-12 17:08:20 +0800

    两种用户都可以注册了

--- src/main/java/r4g19/offer100/model/cjs/SmsCode.java
+++ src/main/java/r4g19/offer100/model/cjs/SmsCode.java
@@ -12,28 +12,31 @@ import java.sql.Timestamp;
 public class SmsCode {
     private String code;
     private Timestamp time;
+    private  String phone;
 
     public String getJSON() {
         return String.format("{\"code\":\"%s\"}", code);
     }
 
-    public SmsCode(String code) {
+    public SmsCode(String code,String phone) {
         this.code = code;
+        this.phone = phone;
         this.time = new Timestamp(System.currentTimeMillis());
     }
 
-    public static SmsCode generate() {
-        return new SmsCode(String.valueOf(((int) (Math.random() * 1000000))), new Timestamp(System.currentTimeMillis()));
+    public static SmsCode generate(String phone) {
+        return new SmsCode(String.valueOf(((int) (Math.random() * 1000000))), new Timestamp(System.currentTimeMillis()),phone);
     }
 
     public boolean equals(SmsCode smsCode) {
+        if(!this.phone.equals(smsCode.getPhone())) return false;
         if (Math.abs(this.time.getTime() - smsCode.time.getTime()) < 300000)
             return this.getCode().equals(smsCode.getCode());
         return false;
     }
 
-    public boolean equals(String smsCode) {
-        return this.equals(new SmsCode(smsCode));
+    public boolean equals(String smsCode,String  phone) {
+        return this.equals(new SmsCode(smsCode,phone));
     }
 
     public int hashCode(){
