4b766b5cb406a2290768de87465d4d3fd363807a
cym <16201235@stu.nchu.edu.cn>
2019-10-17 14:47:17 +0800

    fix for db struct update

--- src/main/java/r4g19/offer100/api/zhj/Job.java
+++ src/main/java/r4g19/offer100/api/zhj/Job.java
@@ -1,17 +1,12 @@
 package r4g19.offer100.api.zhj;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 import r4g19.offer100.annotations.cym.API;
 import r4g19.offer100.api.APIBase;
 import r4g19.offer100.jooq.tables.pojos.Collection;
 import r4g19.offer100.jooq.tables.pojos.Hiring;
 import r4g19.offer100.service.zhj.JobService;
 
-import javax.servlet.http.HttpServletResponse;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
@@ -37,7 +32,7 @@ public class Job extends APIBase {
     public String addToCollection(Hiring hiring, @Context SecurityContext securityContext) {
         String u = APIBase.getUsername(securityContext);
         //u = "1";
-        Collection collection = new Collection(u,hiring.getUsername(),hiring.getId(),1,
+        Collection collection = new Collection(u, hiring.getId(), 1,
                 new Timestamp(new Date().getTime()), 0);
         jobService.manageCollectionJob(collection, JobService.ADD);
         return "success";
